518b1a3112a164e0e7ecbb8f25dce63b6a041bb0
andy <andy@andydote.co.uk>
2014-03-08 19:27:17 +0000

    bus: no point making it instance based, only having one

--- bus.lua
+++ bus.lua
@@ -1,45 +1,37 @@
 local addon, ns = ...
 
+local subscribers = {}
+
 local bus = {
 
-	new = function()
+	subscribe = function(messageType, handler)
 
-		local this = {}
-		local subscribers = {}
-
-		this.subscribe = function(messageType, handler)
-
-			subscribers[messageType] = subscribers[messageType] or {}
-			subscribers[messageType][handler] = true
-
-		end
-
-		this.unsubscribe = function(messageType, handler)
-
-			if subscribers[messageType] and subscribers[messageType][handler] then
-				subscribers[messageType][handler] = nil
-			end
-
-		end
-
-		this.push = function(messageType, ...)
-
-			local handlers = subscribers[messageType]
-
-			if not handlers then
-				return
-			end
-
-			for handler, active in pairs(handlers) do
-				handler(...)
-			end
-
-		end
-
-		return this
+		subscribers[messageType] = subscribers[messageType] or {}
+		subscribers[messageType][handler] = true
 
 	end,
 
+	unsubscribe = function(messageType, handler)
+
+		if subscribers[messageType] and subscribers[messageType][handler] then
+			subscribers[messageType][handler] = nil
+		end
+
+	end,
+
+	push = function(messageType, ...)
+
+		local handlers = subscribers[messageType]
+
+		if not handlers then
+			return
+		end
+
+		for handler, active in pairs(handlers) do
+			handler(...)
+		end
+
+	end,
 }
 
 ns.bus = bus
