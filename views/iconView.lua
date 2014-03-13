4318535f06e0f93b95a903737e263a70c8ea5231
andy <andy@andydote.co.uk>
2014-03-13 00:07:16 +0000

    views.iconView: add default constructor

--- views/iconView.lua
+++ views/iconView.lua
@@ -4,6 +4,14 @@ local iconView = ns.view:new({
 
 	type = "icon",
 
+	new = function(self)
+
+		local this = {}
+		setmetatable(this, { __index = self })
+
+		return this
+
+	end,
 })
 
 ns.viewFactory.add(iconView)
