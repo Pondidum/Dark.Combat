ef2081311fdb8cdb545fc7a02a163c506bba3687
andy <andy@andydote.co.uk>
2014-03-15 21:55:52 +0000

    cooldownMonitor: add cooldown tracking

---
+++ monitors/cooldownMonitor.lua
@@ -0,0 +1,65 @@
+local addon, ns = ...
+local bus = ns.bus
+local events = ns.lib.events:new()
+
+local auraMonitor = ns.monitor:new({
+
+	new = function(self)
+
+		local this = {}
+		setmetatable(this, { __index = self })
+
+		this.type = "cooldown"
+		this.cooldowns = {}
+		this.textures = {}
+
+		return this
+
+	end,
+
+	enable = function(self)
+
+		local textures = self.tetures
+		local cooldowns = self.cooldowns
+
+		local message = {}
+
+		local update = function(eventStore, event, unitID)
+
+			for i, spellName in pairs(cooldowns) do
+
+				local start, duration, enable = GetSpellCooldown(spellName)
+
+				table.wipe(message)
+
+				message.spellName = spellName
+				message.name = spellName
+				message.texture = tetures[spellName]
+				message.start = start
+				message.duration = duration
+				message.finish = (start or 0) - (duration or 0)
+
+				bus.push("cooldownChanged", message)
+
+			end
+
+
+		end
+
+		events.register("ACTIONBAR_UPDATE_COOLDOWN", update)
+		events.register("SPELL_UPDATE_USABLE", update)
+
+	end,
+
+	register = function(self, unit, spellID, spellFilter)
+
+		local spellName, rank, texture = GetSpellInfo(spellID)
+
+		self.cooldowns[spellID] = spellName
+		self.textures[spellID] = texture
+
+	end,
+
+})
+
+ns.monitorFactory.add(auraMonitor)
