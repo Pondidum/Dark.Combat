3ce1ce78af008f7edf5a0df4827a909884e9ea68
andy <andy@andydote.co.uk>
2014-03-16 17:52:43 +0000

    run: change monitorFactory to use spellIDs rather than spellNames

--- run.lua
+++ run.lua
@@ -2,7 +2,7 @@ local addon, ns = ...
 
 local run = function()
 
-	ns.monitorFactory.startType("aura", "player", "Lightning Shield")
+	ns.monitorFactory.startType("aura", "player", 324) --"Lightning Shield"
 
 	-- ns.bus.subscribe("auraChanged", function(args)
 	-- 	print("AuraChanged:", args.spellName, args.name, args.start, args.duration, args.finish)
