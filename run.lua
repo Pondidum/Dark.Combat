521bdcf70a9faa597c1e68528f9871a44bc8c5cf
andy <andy@andydote.co.uk>
2014-03-16 12:51:26 +0000

    run: add test view

--- run.lua
+++ run.lua
@@ -4,9 +4,9 @@ local run = function()
 
 	ns.monitorFactory.startType("aura", "player", "Lightning Shield")
 
-	ns.bus.subscribe("auraChanged", function(args)
-		print("AuraChanged:", args.spellName, args.name, args.start, args.duration, args.finish)
-	end)
+	-- ns.bus.subscribe("auraChanged", function(args)
+	-- 	print("AuraChanged:", args.spellName, args.name, args.start, args.duration, args.finish)
+	-- end)
 
 end
 
