3170145f9ae2abc155ab999a03b647e0f1f8a86c
andy <andy@andydote.co.uk>
2014-03-17 22:53:48 +0000

    run: use a single config row now

--- run.lua
+++ run.lua
@@ -2,12 +2,14 @@ local addon, ns = ...
 
 local run = function()
 
+	local config = { spellID = 16166, controller = "cooldownLifetime" }
+
 	local view = ns.viewFactory.getInstance("icon")
 	view:init(UIParent)
 	view.frame:SetPoint("LEFT", UIParent, "LEFT", 20, 0)
 	view.frame:SetSize(25, 25)
 
-	local controller = ns.controllerFactory.create("cooldownLifetime", view, { spellID = 16166 })
+	local controller = ns.controllerFactory.create(config.controller, view, config)
 
 	for i,v in ipairs(controller:getMonitorConfigs()) do
 		ns.monitorFactory.startType(unpack(v))
