954de2b2d71811559c4b1b1f13d40113affb5423
andy <andy@andydote.co.uk>
2014-03-22 18:40:12 +0000

    groups: add iconbarGroup

---
+++ groups/iconbarGroup.lua
@@ -0,0 +1,38 @@
+local addon, ns = ...
+
+local layout = ns.lib.layout
+
+local iconbarGroup = ns.group:new({
+
+	type = "iconbar",
+
+	new = function(self)
+
+		local this = {}
+		setmetatable(this, { __index = self })
+
+		return this
+
+	end,
+
+	init = function(self, config)
+
+		local frame = CreateFrame("Frame", config.name, UIParent)
+		frame:SetPoint(unpack(config.position))
+		layout.init(frame, {
+			type = "HORIZONTAL",
+			autosize = true,
+			marginLeft = 4,
+			marginRight = 4,
+			defaultChildWidth = 32,
+			defaultChildHeight = 32,
+			forceChildSize = true,
+		})
+
+		self.frame = frame
+
+	end,
+
+})
+
+ns.groupFactory.add(iconbarGroup)
