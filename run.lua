d7f20a29f37b6e3b8e07a43521d3d389043e7cd9
andy <andy@andydote.co.uk>
2014-03-22 18:56:41 +0000

    run: expand to add views to groups

--- run.lua
+++ run.lua
@@ -2,13 +2,10 @@ local addon, ns = ...
 
 local run = function()
 
-	--local config = { spellID = 16166, controller = "cooldownLifetime" }
-	--local config = { macroName = "EnhUnleash", controller = "macro" }
 	local config = { spellID = 53817, glowOn = 5, controller = "aura" }
 
 	local view = ns.viewFactory.getInstance("icon")
 	view:init(UIParent)
-	view.frame:SetPoint("LEFT", UIParent, "LEFT", 20, 0)
 	view.frame:SetSize(25, 25)
 
 	local controller = ns.controllerFactory.create(config.controller, view, config)
@@ -17,8 +14,25 @@ local run = function()
 		ns.monitorFactory.startType(unpack(v))
 	end
 
-
 	controller:enable()
+
+
+	local groups = {
+		{
+			name = "rotation",
+			type = "iconbar",
+			position = {"CENTER", "UIParent", "CENTER", 0, -150},
+		},
+	}
+
+	for name, groupConfig in pairs(groups) do
+
+		local group = ns.groupFactory.create(groupConfig.type)
+		group:init(groupConfig)
+
+		group:addChild(view)
+
+	end
 
 end
 
