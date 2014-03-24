a10413d794b9b9beb64ed77939f8db6b81eac9c5
andy <andy@andydote.co.uk>
2014-03-24 23:02:03 +0000

    run: add hacky initialisation for testing with

--- run.lua
+++ run.lua
@@ -1,38 +1,94 @@
 local addon, ns = ...
+local config = ns.config
+local events = ns.lib.events:new()
 
 local run = function()
 
-	local config = { spellID = 53817, glowOn = 5, controller = "aura" }
+	local activeGroups = {}
+	local activeControllers = {}
 
-	local view = ns.viewFactory.getInstance("icon")
-	view:init(UIParent)
-	view.frame:SetSize(25, 25)
-
-	local controller = ns.controllerFactory.create(config.controller, view, config)
-
-	for i,v in ipairs(controller:getMonitorConfigs()) do
-		ns.monitorFactory.startType(unpack(v))
-	end
-
-	controller:enable()
-
-
-	local groups = {
-		{
-			name = "rotation",
-			type = "iconbar",
-			position = {"CENTER", "UIParent", "CENTER", 0, -150},
-		},
-	}
-
-	for name, groupConfig in pairs(groups) do
+	for i, groupConfig in pairs(config.groups) do
 
 		local group = ns.groupFactory.create(groupConfig.type)
 		group:init(groupConfig)
 
-		group:addChild(view)
+		activeGroups[groupConfig.name] = group
 
 	end
+
+	local cleanOldControllersAndViews = function()
+
+		for i,v in ipairs(activeControllers) do
+			v:disable()
+		end
+
+		table.wipe(activeControllers)
+
+		for name, group in pairs(activeGroups) do
+			group:clearViews()
+		end
+
+		ns.viewFactory.recycleAll()
+
+	end
+
+	local getSpecConfig = function()
+
+		local _, class = UnitClass("player")
+
+		if not config.classes[class] then
+			return
+		end
+
+		local specID = GetSpecialization()
+
+		if not specID then
+			return
+		end
+
+		local id, name = GetSpecializationInfo(specID)
+		local specConfig = config.classes[class][name]
+
+		return specConfig
+
+	end
+
+	local onSpecChanged = function()
+
+		cleanOldControllersAndViews()
+
+		local specConfig = getSpecConfig()
+
+		if not specConfig then
+			return
+		end
+
+		for groupName, children in pairs(specConfig) do
+
+			local group = activeGroups[groupName]
+
+			for i, childConfig in ipairs(children) do
+
+				local view = group:createView()
+				local controller = ns.controllerFactory.create(childConfig.controller, view, childConfig)
+
+				for i,v in ipairs(controller:getMonitorConfigs()) do
+					ns.monitorFactory.startType(unpack(v))
+				end
+
+				table.insert(activeControllers, controller)
+
+			end
+
+		end
+
+		for i,v in ipairs(activeControllers) do
+			v:enable()
+		end
+
+	end
+
+	events.register("SPELLS_CHANGED", onSpecChanged)
 
 end
 
