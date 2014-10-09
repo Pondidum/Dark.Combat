151502b6df3b519dd6cd6c7860e7f90b1069ca2f
andy <andy@andydote.co.uk>
2014-10-09 17:50:36 +0100

    compilers: generalise compilers, use inheritance

--- compiler.lua
+++ compiler.lua
@@ -1,63 +1,63 @@
-local addon, ns = ...
-
-
-local addRange = function(accumulator, items)
-	for id, data in pairs(items) do
-		accumulator[id] = data
-	end
-end
-
-local removeRange = function(accumulator, items)
-	for id, data in pairs(items) do
-		accumulator[id] = nil
-	end
-end
-
-local blacklistCompiler = function(domain, unit, playerClass, playerSpec)
-
-	unit, playerClass, playerSpec = unit:upper(), playerClass:upper(), playerSpec:upper()
-
-	local blacklist = {}
-
-	addRange(blacklist, domain.globalBlack)
-	removeRange(blacklist, domain.globalWhite)
-
-	addRange(blacklist, domain:get("unitBlack", unit))
-	removeRange(blacklist, domain:get("unitWhite", unit))
-
-	addRange(blacklist, domain:get("classBlack", unit, playerClass))
-	removeRange(blacklist, domain:get("classWhite", unit, playerClass))
-
-	addRange(blacklist, domain:get("specBlack", unit, playerClass, playerSpec))
-	removeRange(blacklist, domain:get("specWhite", unit, playerClass, playerSpec))
-
-	return blacklist
-end
-
-local whitelistCompiler = function(domain, unit, playerClass, playerSpec)
-
-	unit, playerClass, playerSpec = unit:upper(), playerClass:upper(), playerSpec:upper()
-
-	local whitelist = {}
-
-	addRange(whitelist, domain.globalWhite)
-	removeRange(whitelist, domain.globalBlack)
-
-	addRange(whitelist, domain:get("unitWhite", unit))
-	removeRange(whitelist, domain:get("unitBlack", unit))
-
-	addRange(whitelist, domain:get("classWhite", unit, playerClass))
-	removeRange(whitelist, domain:get("classBlack", unit, playerClass))
-
-	addRange(whitelist, domain:get("specWhite", unit, playerClass, playerSpec))
-	removeRange(whitelist, domain:get("specBlack", unit, playerClass, playerSpec))
-
-	return whitelist
-end
+local addon, ns = ... or {}, {}
 
 local compiler = {
-	compileWhite = whitelistCompiler,
-	compileBlack = blacklistCompiler
+
+	extend = function(self, this)
+		return setmetatable(this, { __index = self })
+	end,
+
+	new = function(self)
+
+		local this = {}
+		this.accumulator = {}
+
+		return setmetatable(this, { __index = self})
+
+	end,
+
+	addItems = function(self, black, white)
+
+	end,
+
+	compile = function(self)
+		return self.accumulator
+	end,
+
+	addRange = function(self, items)
+		for id, data in pairs(items) do
+			self.accumulator[id] = data
+		end
+	end,
+
+	removeRange = function(self, items)
+		for id, data in pairs(items) do
+			self.accumulator[id] = nil
+		end
+	end,
 }
 
-ns.compiler = compiler
+local blacklistCompiler = compiler:extend({
+
+	addItems = function(self, black, white)
+
+		self:addRange(black)
+		self:removeRange(white)
+
+	end,
+})
+
+local whitelistCompiler = compiler:extend({
+
+	addItems = function(self, black, white)
+
+		self:addRange(white)
+		self:removeRange(black)
+
+	end,
+})
+
+ns.compilers = {
+	base = compiler,
+	black = blacklistCompiler,
+	white = whitelistCompiler
+}
