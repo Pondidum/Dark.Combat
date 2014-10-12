e088835a697b8631b046571e5fb418f7ecb44c8e
andy <andy@andydote.co.uk>
2014-10-12 16:01:22 +0100

    run: read config in, expose objects globally for testing

--- run.lua
+++ run.lua
@@ -1,12 +1,23 @@
 local addon, ns = ...
 
 local domain = ns.domain
+local infrastructure = ns.infrastructure
 
 local run = function()
 
 	local talents = domain.talentCache:new()
 	local cdDomain = domain.cooldownDomain:new(talents)
+
+	local reader = infrastructure.configReader:new(cdDomain)
+	reader:read(ns.config.cooldowns)
+
 	local orchestrator = ns.orchestrator:new(cdDomain)
+
+	Dark.combat = {
+		talents = talents,
+		cdDomain = cdDomain,
+		orchestrator = orchestrator
+	}
 
 end
 
