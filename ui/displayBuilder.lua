f8a87753edaae16bc8a1de88db789f8af0ee2a5e
andy <andy@andydote.co.uk>
2014-10-14 19:33:38 +0100

    displayBuilder: prototype functionality

--- ui/displayBuilder.lua
+++ ui/displayBuilder.lua
@@ -1,34 +1,72 @@
 local addon, ns = ...
 
+local layout = ns.lib.layout
+local cache = ns.lib.cache
+
 local displayBuilder = {
 
 	new = function(self)
 
 		local this = {}
+
 		this.configs = {}
-		this.displays = {}
+		this.containers = {}
+		this.displays = cache.new(function(i) self:createView(i))
 
 		setmetatable(this, { __index = self })
 		return this
 
 	end,
 
-	configureDisplay = function(self, name)
-		self.configs[name] = {}
+	configureDisplay = function(self, name, points)
+
+		self.configs[name] = {
+			points = points
+		}
+
 	end,
 
 	createDisplays = function(self, displaySpells)
 
+		local views = self.cache
+		views.recycleAll()
+
 		for displayName, spells in pairs(displaySpells) do
 
 			local config = self.configs[displayName]
+			self.containers[displayName] = self.containers[displayName] or self:createDisplay(displayName)
 
-			-- ?
+			self:configureDisplay(self.containers[displayName], config, spells)
+
+			for i, spell in ipairs(spells) do
+				local view = views.get()
+				self:configureView(view)
+
+
+			end
+
 		end
 
 	end,
 
+	createDisplay = function(self, name)
 
+		local frame = CreateFrame("Frame", "DarkCombat" .. displayName, UIParent)
+		layout.init(frame, { autosize = true })
+
+		return frame
+
+	end,
+
+	configureDisplay = function(self, frame, config, spells)
+
+		frame:ClearAllPoints()
+
+		for i, pointConfig in ipairs(config.points) do
+			frame:SetPoint(unpack(pointConfig))
+		end
+
+	end,
 }
 
 ns.ui.displayBuilder = displayBuilder
