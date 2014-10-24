b55ecdd331a7c95ee3dfe7257c31b58060e15b57
andy <andy@andydote.co.uk>
2014-10-24 18:32:18 +0100

    config: add macro tracking

--- config.lua
+++ config.lua
@@ -9,6 +9,7 @@ local config = {
 	cooldowns = {
 
 		{ "trackSpell", "rotation", "SHAMAN", "Enhancement", "spell", 17364 },	-- storm strike
+		{ "trackSpell", "rotation", "SHAMAN", "Enhancement", "macro", "EnhUnleash" },	-- unleash macro of win
 		{ "trackSpell", "rotation", "SHAMAN", "Enhancement", "spell", 53817 },	-- maelstrom weapon
 		{ "trackSpell", "rotation", "SHAMAN", "Enhancement", "spell", 117014 }, -- elemental blast
 		{ "trackSpell", "rotation", "SHAMAN", "Elemental", "spell", 51505 },	-- lava burst
