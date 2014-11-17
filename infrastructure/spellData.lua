2d3f27c523ddfcb0de08c7de8de832509291cdf0
andy <andy@andydote.co.uk>
2014-11-17 19:36:08 +0000

    spellData: add shadow infusion max stacks data

--- infrastructure/spellData.lua
+++ infrastructure/spellData.lua
@@ -3,6 +3,9 @@ local addon, ns = ...
 local charges = {
 	[53817] = 5,
 	["Maelstrom Weapon"] = 5,
+
+	[91342] = 5,
+	["Shadow Infusion"] = 5,
 }
 
 setmetatable(charges, { __index = function(key) return 1 end })
