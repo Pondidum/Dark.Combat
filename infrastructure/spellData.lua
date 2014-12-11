5221c3d4517223a09dbaf6474dab0b7cf370ee3d
andy <andy@andydote.co.uk>
2014-12-11 18:57:07 +0000

    spellData: add facility for useAtStacks

--- infrastructure/spellData.lua
+++ infrastructure/spellData.lua
@@ -1,24 +1,38 @@
 local addon, ns = ...
 
-local charges = {
-	[53817] = 5,
-	["Maelstrom Weapon"] = 5,
-
-	[91342] = 5,
-	["Shadow Infusion"] = 5,
-
-	[114851] = 5,
-	["Blood Charge"] = 5,
+local defaults = {
+	spellID = 0,
+	spellName = "",
+	maxCharges = 0,
+	useAtCharges = 0,
 }
 
-setmetatable(charges, { __index = function(key) return 1 end })
+local defaultsMeta = { __index = defaults }
+
+local byID = {}
+local byName = {}
 
 local spellData = {
 
-	getMaxCharges = function(spellID)
-		return charges[spellID]
+	register = function(info)
+
+		setmetatable(info, defaultsMeta)
+
+		byID[info.spellID] = info
+		byName[info.spellName] = info
+
+	end,
+
+	getMaxCharges = function(spell)
+		local info = byID[spell] or byName[spell] or defaults
+
+		return info.maxCharges
 	end,
 
 }
+
+spellData.register({spellID = 53817, spellName = "Maelstrom Weapon", maxCharges = 5, useAtCharges = 5 })
+spellData.register({spellID = 91342, spellName = "Shadow Infusion", maxCharges = 5, useAtCharges = 5 })
+spellData.register({spellID = 114851, spellName = "Blood Charge", maxCharges = 5, useAtCharges = 5 })
 
 ns.infrastructure.spellData = spellData
