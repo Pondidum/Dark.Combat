3216f586187a6002776b5fc65c458e9da32a26b8
andy <andy@andydote.co.uk>
2014-12-23 22:16:30 +0000

    run: clean up run

--- run.lua
+++ run.lua
@@ -5,26 +5,20 @@ local infrastructure = ns.infrastructure
 
 local run = function()
 
-	--local reader = infrastructure.configReader:new()
-
-
-
 	local talents = domain.talentCache:new()
 	local cdDomain = domain.cooldownDomain:new(talents)
-	cdDomain:readConfig(ns.config.cooldowns)
-	--reader:read(cdDomain, ns.config.cooldowns)
-
 	local displayBuilder = ns.ui.displayBuilder:new()
-	displayBuilder:readConfig(ns.config.displays)
-	--reader:read(displayBuilder, ns.config.displays)
 
+	cdDomain:readConfig(ns.config.cooldowns)
+	displayBuilder:readConfig(ns.config.displays)
 
 	local orchestrator = ns.orchestrator:new(cdDomain, displayBuilder)
 
 	Dark.combat = {
 		talents = talents,
 		cdDomain = cdDomain,
-		orchestrator = orchestrator
+		orchestrator = orchestrator,
+		config = ns.configui,
 	}
 
 end
