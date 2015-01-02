bfd1454cdfd1cd5ca4f6dc6f58c68a95c25632c0
andy <andy@andydote.co.uk>
2015-01-02 14:27:22 +0000

    configui: populate the display editor

--- config/displayEditor.lua
+++ config/displayEditor.lua
@@ -11,17 +11,17 @@ local displayConfig = {
 	wrap = false,
 	autosize = "x",
 	controls = {
-		{ type = "textbox", name = "$parentName" },
+		{ type = "textbox", key = "name" },
 		{ type = "label",	text = "Point" },
-		{ type = "textbox", name = "$parentPoint" },
+		{ type = "textbox", key = "point" },
 		{ type = "label",	text = "target" },
-		{ type = "textbox", name = "$parentTarget" },
+		{ type = "textbox", key = "target" },
 		{ type = "label",	text = "TargetPoint" },
-		{ type = "textbox", name = "$parentTargetPoint" },
+		{ type = "textbox", key = "targetPoint" },
 		{ type = "label",	text = "X Offset" },
-		{ type = "textbox", name = "$parentOffsetX" },
+		{ type = "textbox", key = "offsetX" },
 		{ type = "label",	text = "Y Offset" },
-		{ type = "textbox", name = "$parentOffsetY" },
+		{ type = "textbox", key = "offsetY" },
 	}
 }
 
@@ -32,14 +32,28 @@ local displayComponent = class:extend({
 		displayConfig.name = data.name
 		local component = controls.dsl:single(host, displayConfig)
 
-		self.frame = component.frame
 		self.data = data
+		self.frame = component.frame
+		self.controls = component.controls
 
 		self:populate()
 	end,
 
 	populate = function(self)
 
+		local controls = self.controls
+		local data = self.data
+
+		local name = data.name
+		local point, target, targetPoint, x, y = unpack(data.point)
+		local childWidth, childHeight = unpack(data.childSize)
+
+		controls.name:text(name)
+		controls.point:text(point)
+		controls.target:text(target)
+		controls.targetPoint:text(targetPoint)
+		controls.offsetX:text(x)
+		controls.offsetY:text(y)
 
 	end,
 
