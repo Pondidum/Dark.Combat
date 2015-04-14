ad5478f9071bf3eec5a630fadc833ebe845955b4
andy <andy@andydote.co.uk>
2015-04-14 18:10:25 +0100

    configui: populate spellrow from model

--- ui.config/fullSpellRow.lua
+++ ui.config/fullSpellRow.lua
@@ -9,21 +9,22 @@ local fullSpellRow = class:extend({
 		self.model = model
 	end,
 
-	buildInterface = function(self, model)
+	buildInterface = function(self)
 
+		local model = self.model
 		local container = controls:frame({})
-
 		local children = controls:createDefaults({ parent = container })
 
-		local displayName, classname, specName, type, spellid, auraid = children:series(
+		local displayName, classname, specName, action, spellid, auraid = children:series(
 			children:textbox({ text = model.display }),
 			children:textbox({ text = model.class }),
 			children:textbox({ text = model.spec }),
-			children:textbox({ text = model.type }),
-			-- children:textbox({ text = model.data }),
-			-- children:textbox({}),
+			children:textbox({ text = model.action }),
+			children:textbox({ text = model.data[1] }),
+			children:textbox({ text = model.data[2] }),
 		)
 
+		displayName:SetPoint("LEFT", container, "LEFT", 0, 0)
 
 	end,
 
